791c51e7436b16d7a965e200481e470c9a37b49d
lan.tl <lantranle.06@gmail.com>
2026-05-12 22:41:20 +0700

    inital commit

---
+++ src/test/java/com/lantranle/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lantranle.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
